b7c94be08df75b56e86300ccb9a5b39e53db646a
Rahul Arvind <rahul.a2208@gmail.com>
2024-12-17 14:13:39 -0500

    update_v2

--- model.py
+++ model.py
@@ -32,6 +32,17 @@ from sklearn.model_selection import GridSearchCV
 #Loading the data
 df=pd.read_csv("stroke_data.csv")
 df.head()
+df.tail()
+df.info()
+df.describe()
+df.isnull().sum()
+df.shape
+df.columns
+df.dtypes
+df.nunique()
+df['stroke'].value_counts()
+df['stroke'].value_counts(normalize=True)
+
 
 #%%
 #Shape of the dataset
